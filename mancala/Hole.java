342cd0d40dc3a19b758596ab3015d21b86123180
jethereal <103599216+MeantOwl@users.noreply.github.com>
2022-11-12 10:56:42 -0800

    Update Hole.java

--- mancala/Hole.java
+++ mancala/Hole.java
@@ -1,5 +1,9 @@
 package mancala;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+
 public interface Hole {
 	
 	int getX();
@@ -8,5 +12,9 @@ public interface Hole {
 	int getHeight();
 	void addStone();
 	int getStones();
-
+	void setShape(Shape s);
+	void setColor(Color s);
+	Shape getShape();
+	void draw(Graphics2D g2);
+	
 }
