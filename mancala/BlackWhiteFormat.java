c6e8288803c0de4bba82de43df59952b2ad46e06
jethereal <103599216+MeantOwl@users.noreply.github.com>
2022-11-12 10:57:22 -0800

    Update BlackWhiteFormat.java

--- mancala/BlackWhiteFormat.java
+++ mancala/BlackWhiteFormat.java
@@ -2,7 +2,9 @@ package mancala;
 
 import java.awt.Color;
 import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
 
 /**
  * The BlackWhiteFormat class is a Strategy pattern that styles the board with a black and white theme. 
@@ -27,7 +29,11 @@ public class BlackWhiteFormat implements MancalaFormatter{
 	}
 
 	public Shape formatPitShape(Pit pit) {
-		return new Rectangle2D.Double(pit.getX(), pit.getY(), pit.getWidth(), pit.getHeight());
+		return new Ellipse2D.Double(pit.getX(), pit.getY(), pit.getWidth(), pit.getHeight());
+	}
+
+	public Shape formatGoalMancalaShape(GoalMancala goal) {
+		return new RoundRectangle2D.Double(goal.getX(), goal.getY(), goal.getWidth(), goal.getHeight(), 20, 20);
 	}
 
 }
