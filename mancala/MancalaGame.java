2a7feed45078cd653896d257bca2f35589e31b42
jethereal <103599216+MeantOwl@users.noreply.github.com>
2022-11-18 21:13:46 -0800

    Update MancalaGame.java

--- mancala/MancalaGame.java
+++ mancala/MancalaGame.java
@@ -2,21 +2,26 @@ package mancala;
 
 import java.awt.BorderLayout;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class MancalaGame extends JFrame{
 
 	static final int FRAME_WIDTH = 550;
-	static final int FRAME_HEIGHT = 450;
-	
+	static final int FRAME_HEIGHT = 400;
+
 	private MancalaModel model;
 	private MancalaView view;
-	private MancalaFormatter format;
-	
+
 	public MancalaGame() {
 		model = new MancalaModel();
-		view = new MancalaView(model, new BlueFormat());
+		model.startGame();
+
+		view = new MancalaView(model);
+		model.addChangeListener(view);
 		setLayout(new BorderLayout());
 		add(view, BorderLayout.CENTER); //add view to CENTER of frame
 		
@@ -24,14 +29,7 @@ public class MancalaGame extends JFrame{
 		JButton undoButton = new JButton("UNDO");
 		menuPanel.add(undoButton);		//add undo button to menu panel
 		add(menuPanel, BorderLayout.NORTH);	//add menu panel to NORTH of frame
+		
 	}
-	
-	public void setFormat(MancalaFormatter f) {
-		this.format = f;
-	}
-	
-	public MancalaFormatter getFormat() {
-		return format;
-	}
-	
+
 }
