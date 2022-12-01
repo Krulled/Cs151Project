8a876771cebb943fc13f452f28d6a508d5883651
jethereal <103599216+MeantOwl@users.noreply.github.com>
2022-12-01 11:28:32 -0800

    Update MancalaModel.java

--- mancala/MancalaModel.java
+++ mancala/MancalaModel.java
@@ -1,11 +1,14 @@
 package mancala;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 /**
@@ -17,15 +20,26 @@ public class MancalaModel{
 	private ArrayList<ChangeListener> listeners;
 	private int initialStones;
 	private MancalaFormatter format;
-
+	static String winner = "";
+	
+	private int[] allPits;
+	private int[] previousPits;
+	private int numberOfUndos = 0;
+	
 	
 	public MancalaModel() {
 		listeners = new ArrayList<>();
+		allPits = new int[14];
+		previousPits = new int[14];
+	}
+	
+	public int[] getViewPits() {
+		return MancalaView.getTotalStonesPits();
 	}
 	
 	public void startGame() {
 		JDialog popup = new JDialog();
-
+		
 		JLabel theme = new JLabel("Choose a theme!");
 		Object[] optionsForTheme = {"Blue Format", "Pink Format"};
 		String initialThemeSelection = "Blue Format";
@@ -72,6 +86,12 @@ public class MancalaModel{
 		listeners.add(listener);
 	}
 	
+	public void updateListeners() {
+		for(ChangeListener listener : listeners) {
+			listener.stateChanged(new ChangeEvent(this));
+		}
+	}
+	
 	public void setFormat(MancalaFormatter f) {
 		this.format = f;
 	}
@@ -87,6 +107,131 @@ public class MancalaModel{
 	public void setInitialStones(int initialStones) {
 		this.initialStones = initialStones;
 	}
+	
+	public int[] getAllPits(){
+		return this.allPits;
+	}
+	
+	public void moveStones(int index) {
+		previousPits =  getViewPits().clone();
+		allPits = getViewPits();
+		int[] stonesInPits = MancalaView.getTotalStonesPits();
+		numberOfUndos = 0;
+		
+		int numberStones = stonesInPits[index]; //number of stones in THIS pit
+		stonesInPits[index] = 0; //reset this pit to 0 stones
+		int temp = index;
+		while(numberStones > 0) { //while number of stones > 0
+			temp++;
+			temp = temp % allPits.length;
+			if(index <= 5 && temp == 13) { //Don't add in MancalaB if Player 1
+				temp = 0;
+			}
+			if(index >= 7 && temp == 6) { //Don't add in MancalaA if Player 2
+				temp = 7;
+			}
+			stonesInPits[temp] = stonesInPits[temp] + 1;
+			numberStones--;
+		}
+		if(getEmptyRow() == "Row A") {
+			giveAllToMancalaB();
+		}
+		else if(getEmptyRow() == "Row B") {
+			giveAllToMancalaA();
+		}
+		this.updateListeners();
 
+		if(checkIfGameEnds() == true) {
+			displayWinner();
+		}
+	}
+	
+	public boolean checkIfGameEnds() {
+		boolean allEmptyPits = true;
+		int[] stonesInPits = MancalaView.getTotalStonesPits();
+		for(int i = 0; i < stonesInPits.length; i++) {
+			if(i != 6 && i != 13) {
+				if(stonesInPits[i] != 0) {
+					allEmptyPits = false;
+				}
+			}
+		}
+		return allEmptyPits;
+	}
+	
+	public void displayWinner() {
+		if(allPits[6] > allPits[13]) {
+			winner = "Player A";
+		}
+		else if(allPits[6] < allPits[13]) {
+			winner = "Player B";
+		}
+		else {
+			winner = "It's a tie!";
+		}
+		MancalaView.signalGameEnd();
+	}
+
+	public void undoMove() { 
+		numberOfUndos++;
+		allPits = previousPits.clone();
+		this.updateListeners();
+	}
+	
+	public JButton getUndoButton() {
+		JButton undoButton = new JButton("UNDO");
+		undoButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(numberOfUndos > 3) {
+					return;
+				}
+				undoMove();
+			}
+		});
+		return undoButton;
+	}
+
+	public String getEmptyRow() {
+		boolean rowBEmpty = true;
+		boolean rowAEmpty = true;
+		for(int i = 0; i < 6; i++) { //Check ROW A
+			if(allPits[i] != 0) { 
+				rowAEmpty = false;
+			}
+		}
+		for(int i = 7; i < 13; i++) { //Check ROW B
+			if(allPits[i] != 0) {
+				rowBEmpty = false;
+			}
+		}
+		if(rowBEmpty == true && rowAEmpty == false) { //Only Row B is empty
+			return "Row B"; //all stones should go to Mancala A
+		}
+		if(rowBEmpty == false && rowAEmpty == true) { //Only Row A is empty
+			return "Row A"; //all stones should go to Mancala B
+		}
+		return "Both aren't empty"; //Both rows are empty
+	}
+	
+	public void giveAllToMancalaA() {
+		int stonesLeftInRowA = 0;
+		for(int i = 0; i < 6; i++) {
+			stonesLeftInRowA += allPits[i];
+			allPits[i] = 0;
+		}
+		allPits[6] += stonesLeftInRowA;
+		this.updateListeners();
+	}
+	
+	public void giveAllToMancalaB() {
+		int stonesLeftInRowB = 0;
+		for(int i = 7; i < 13; i++) {
+			stonesLeftInRowB += allPits[i];
+			allPits[i] = 0;
+		}
+		allPits[13] += stonesLeftInRowB; //add all REMAINING stones to Mancala B
+		this.updateListeners();
+
+	}
 
 }
