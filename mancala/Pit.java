50501493b3e7cadff1dd7ae5da0fc93095079f88
jethereal <103599216+MeantOwl@users.noreply.github.com>
2022-12-01 11:29:58 -0800

    Update Pit.java

--- mancala/Pit.java
+++ mancala/Pit.java
@@ -2,6 +2,7 @@ package mancala;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
@@ -13,9 +14,12 @@ public class Pit implements Hole{
 	private int width;
 	private int height;
 	private int totalStones;
+	private final int STONE_SIZE = 10;
+	private int id;
+	
 	private Shape shape;
 	private Color color;
-	private final int STONE_SIZE = 10;
+	private Color stoneColor;
 	private ArrayList<Ellipse2D.Double> stonesInPit;
 	
 	public Pit(int x, int y, int w, int h) {
@@ -25,8 +29,28 @@ public class Pit implements Hole{
 		this.height = h;
 		stonesInPit = new ArrayList<>();
 		totalStones = 0;
+		id = -1;
 	}
 	
+	public String toString() {
+		String letter = "";
+		if(this.y == MancalaView.TOP_Y) {
+			letter = "B"; 
+		}
+		else if(this.y == MancalaView.BOTTOM_Y){
+			letter = "A"; 
+		}
+		return "Pit ID: " + this.getId() + "\n" + "Pit Label: " + letter + (id+1);
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public int getX() {
 		return this.x;
 	}
@@ -71,9 +95,13 @@ public class Pit implements Hole{
 		this.color = color;
 	}
 	
+	public void setStoneColor(Color stoneColor) {
+		this.stoneColor = stoneColor;
+	}
+	
 	public void draw(Graphics2D g2) {
-		g2.draw(shape);
 		g2.setColor(color);
+		g2.draw(shape);
 		
 		//Display pit label
 		int temp = MancalaView.PIT_WIDTH*2 + MancalaView.PIT_WIDTH/5;
@@ -94,46 +122,47 @@ public class Pit implements Hole{
 			if(this.x == temp+55*5) g2.drawString("A6", x+20, y+MancalaView.PIT_HEIGHT+15);
 		}
 		
-		//Print number of stones in pit 
+		//Display number of stones in pit
 		if(this.y == MancalaView.TOP_Y) {
 			g2.drawString(" " + totalStones, x+20, y+MancalaView.PIT_HEIGHT+15); 
-			g2.setColor(color);
 		}
 		if(this.y == MancalaView.BOTTOM_Y) {
 			g2.drawString(" " + totalStones, x+20, y-10);
-			g2.setColor(color);
 		}
 		
-		//Display stones in pit (NOT FINISHED)
+		//Display stones in pit 
 		int counter = 1; //counter for number of stones already displayed in pit
-		double xcoord = this.x + 10;
-		double ycoord = this.y + 10;
+		double xcoord = this.x;
+		double ycoord = this.y;
+		stonesInPit.clear();
 		for(int i = 0; i < totalStones; i++) {
-			System.out.println(i);
-			if(counter > 10) { //stop displaying stones over 10
+			if(counter > 8) { //stop displaying stones over 8
 				return;
 			}
-			if(counter % 2 == 0) {
-				xcoord = this.x + 10;
-				ycoord = this.y + 25;
-			}
-			else if(counter % 2 == 1) {
+			if(counter % 2 == 0) { 		//EVEN STONE
 				xcoord = this.x + 30;
-				ycoord = this.y + 20;
+				ycoord = (this.y + 10) + 6 * counter/1.8;
 			}
-			Ellipse2D.Double stone = new Ellipse2D.Double(xcoord, ycoord, STONE_SIZE, STONE_SIZE);
-			System.out.println("xcoord: " + xcoord);
-			System.out.println("ycoord: " + ycoord);
+			else if(counter % 2 == 1) { //ODD STONE
+				xcoord = this.x + 20;
+				ycoord = (this.y + 10) + 6 * counter/1.8;
+			}
 
+			Ellipse2D.Double stone = new Ellipse2D.Double(xcoord, ycoord, STONE_SIZE, STONE_SIZE);
+			
 			stonesInPit.add(stone);
-			g2.setColor(Color.CYAN);
+			g2.setColor(stoneColor);
 			g2.fill(stone);
-			g2.draw(stone);
 			g2.setColor(color);
+			g2.draw(stone);
 			counter++;
-			xcoord = this.x + 25;
-			ycoord = this.y + 15;
-		}
-		
-	}	
+//			xcoord = this.x + 20;
+//			ycoord = this.y + 20;
+		}	
+	}
+
+	public boolean contains(Point p) {
+		return shape.contains(p);
+	}
+	
 }
