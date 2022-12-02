151ddbb1a77daa34313b7fe00bcdc4c65dee4d62
jethereal <103599216+MeantOwl@users.noreply.github.com>
2022-12-02 10:55:27 -0800

    Update MancalaGame.java

--- mancala/MancalaGame.java
+++ mancala/MancalaGame.java
@@ -11,11 +11,13 @@ import javax.swing.JPanel;
 public class MancalaGame extends JFrame{
 
 	static final int FRAME_WIDTH = 550;
-	static final int FRAME_HEIGHT = 400;
-
+	static final int FRAME_HEIGHT = 350;
+	
 	private MancalaModel model;
 	private MancalaView view;
-
+/**
+	 * Constructor that initializes instance variables and creates the buttons and panels
+	 */
 	public MancalaGame() {
 		model = new MancalaModel();
 		model.startGame();
@@ -29,6 +31,21 @@ public class MancalaGame extends JFrame{
 		JButton undoButton = model.getUndoButton();
 		menuPanel.add(undoButton);		//add undo button to menu panel
 		add(menuPanel, BorderLayout.NORTH);	//add menu panel to NORTH of frame
+		
+		JPanel themes = new JPanel();
+		JButton blueFormat = model.getBlueFormat();
+		JButton pinkFormat = model.getPinkFormat();
+		themes.add(pinkFormat);
+		themes.add(blueFormat);
+		add(themes, BorderLayout.SOUTH);
+	}
+	
+	/**
+	 * Returns this MancalaGame object
+	 * @return MancalaGame
+	 */
+	public MancalaGame getGame() {
+		return this;
 	}
 
 }
